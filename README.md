5ea78f831dd0a4173b2c2148c54f079f54e421a0
filiks <runlevel07@gmail.com>
2020-02-02 19:00:36 +0100

    working with json router on port 3005

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 project_nba
+
+
+
+start the node-js json server:
+
+json-server --watch db.json --port 3005
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Routes from './routes'
+// import './index.css'
+
+// import App from '.App'
+
+
+ReactDOM.render(<Routes />, document.getElementById('root'));
